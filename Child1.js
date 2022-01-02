8aefaccb91744926df83e751e6337c28596a17c2
vishaldutt457 <duttvishal689@gmail.com>
2022-01-02 19:51:30 +0530

    childs

---
+++ Child1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Child1() {
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default Child1
---
+++ Child2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Child2() {
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default Child2
